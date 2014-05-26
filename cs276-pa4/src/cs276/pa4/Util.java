431f15fc050f2c2269fbfcea48a6fd578895700a
Kavin Yao <kavinyao@gmail.com>
2014-05-25 19:28:56 -0700

    don't throw exception when loading data

--- cs276-pa4/src/cs276/pa4/Util.java
+++ cs276-pa4/src/cs276/pa4/Util.java
@@ -1,9 +1,6 @@
 package cs276.pa4;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -12,72 +9,77 @@ import java.util.stream.Collectors;
 
 public class Util {
     private static double N = 98998;
-    public static Map<Query, Map<String, Document>> loadTrainData(String feature_file_name) throws Exception {
+    public static Map<Query, Map<String, Document>> loadTrainData(String feature_file_name) {
         File feature_file = new File(feature_file_name);
         if (!feature_file.exists()) {
             System.err.println("Invalid feature file name: " + feature_file_name);
             return null;
         }
 
-        BufferedReader reader = new BufferedReader(new FileReader(feature_file));
-        String line = null, url= null, anchor_text = null;
-        Query currentQuery = null;
-        Map<String, Document> currentDocuments = null;
-        Document currentDocument = null;
-
         /* feature dictionary: Query -> (url -> Document)  */
         Map<Query, Map<String, Document>> queryDict = new HashMap<>();
 
-        while ((line = reader.readLine()) != null) {
-            String[] tokens = line.split(":", 2);
-            String key = tokens[0].trim();
-            String value = tokens[1].trim();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(feature_file));
+            String line = null, url = null, anchor_text = null;
+            Query currentQuery = null;
+            Map<String, Document> currentDocuments = null;
+            Document currentDocument = null;
 
-            if (key.equals("query")) {
-                currentQuery = new Query(value.trim());
-                currentDocuments = new HashMap<>();
-                queryDict.put(currentQuery, currentDocuments);
-            } else if (key.equals("url")) {
-                url = value.trim();
-                currentDocument = new Document(url);
-                currentDocuments.put(url, currentDocument);
-            } else if (key.equals("title")) {
-                currentDocument.setTitle(value);
-            } else if (key.equals("header")) {
-                currentDocument.addHeader(value);
-            } else if (key.equals("body_hits")) {
-                String[] temp = value.split(" ", 2);
-                String term = temp[0].trim();
-                List<Integer> positions = Arrays.asList(temp[1].trim().split(" "))
-                        .stream()
-                        .map(pos -> Integer.parseInt(pos))
-                        .collect(Collectors.toList());
+            while ((line = reader.readLine()) != null) {
+                String[] tokens = line.split(":", 2);
+                String key = tokens[0].trim();
+                String value = tokens[1].trim();
 
-                currentDocument.addBodyHits(term, positions);
-            } else if (key.equals("body_length")) {
-                currentDocument.setBodyLength(Integer.parseInt(value));
-            } else if (key.equals("pagerank")) {
-                currentDocument.setPageRank(Integer.parseInt(value));
-            } else if (key.equals("anchor_text")) {
-                anchor_text = value.trim();
-            } else if (key.equals("stanford_anchor_count")) {
-                currentDocument.addAnchor(anchor_text, Integer.parseInt(value));
+                if (key.equals("query")) {
+                    currentQuery = new Query(value.trim());
+                    currentDocuments = new HashMap<>();
+                    queryDict.put(currentQuery, currentDocuments);
+                } else if (key.equals("url")) {
+                    url = value.trim();
+                    currentDocument = new Document(url);
+                    currentDocuments.put(url, currentDocument);
+                } else if (key.equals("title")) {
+                    currentDocument.setTitle(value);
+                } else if (key.equals("header")) {
+                    currentDocument.addHeader(value);
+                } else if (key.equals("body_hits")) {
+                    String[] temp = value.split(" ", 2);
+                    String term = temp[0].trim();
+                    List<Integer> positions = Arrays.asList(temp[1].trim().split(" "))
+                            .stream()
+                            .map(pos -> Integer.parseInt(pos))
+                            .collect(Collectors.toList());
+
+                    currentDocument.addBodyHits(term, positions);
+                } else if (key.equals("body_length")) {
+                    currentDocument.setBodyLength(Integer.parseInt(value));
+                } else if (key.equals("pagerank")) {
+                    currentDocument.setPageRank(Integer.parseInt(value));
+                } else if (key.equals("anchor_text")) {
+                    anchor_text = value.trim();
+                } else if (key.equals("stanford_anchor_count")) {
+                    currentDocument.addAnchor(anchor_text, Integer.parseInt(value));
+                }
             }
+
+            // finish build documents
+            queryDict.values()
+                    .stream()
+                    .flatMap(ds -> ds.values().stream())
+                    .forEach(d -> d.end());
+
+            reader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            queryDict = null;
         }
-
-        // finish build documents
-        queryDict.values()
-                .stream()
-                .flatMap(ds -> ds.values().stream())
-                .forEach(d -> d.end());
-
-        reader.close();
 
         return queryDict;
     }
 
     public static Map<String, Double> loadDFs(String dfFile) throws IOException {
-        Map<String, Double> dfs = new HashMap<String, Double>();
+        Map<String, Double> dfs = new HashMap<>();
 
         BufferedReader br = new BufferedReader(new FileReader(dfFile));
         String line;
@@ -92,8 +94,8 @@ public class Util {
     }
 
     /* query -> (url -> score) */
-    public static Map<String, Map<String, Double>> loadRelData(String rel_file_name) throws IOException {
-        Map<String, Map<String, Double>> result = new HashMap<String, Map<String, Double>>();
+    public static Map<String, Map<String, Double>> loadRelData(String rel_file_name) {
+        Map<String, Map<String, Double>> result = new HashMap<>();
 
         File rel_file = new File(rel_file_name);
         if (!rel_file.exists()) {
@@ -101,38 +103,39 @@ public class Util {
             return null;
         }
 
-        BufferedReader reader = new BufferedReader(new FileReader(rel_file));
-        String line = null, query = null, url = null;
-        int numQuery = 0;
-        int numDoc = 0;
-        while ((line = reader.readLine()) != null) {
-            String[] tokens = line.split(":", 2);
-            String key = tokens[0].trim();
-            String value = tokens[1].trim();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(rel_file));
+            String line = null, query = null, url = null;
+            int numQuery = 0;
+            int numDoc = 0;
+            while ((line = reader.readLine()) != null) {
+                String[] tokens = line.split(":", 2);
+                String key = tokens[0].trim();
+                String value = tokens[1].trim();
 
-            if (key.equals("query")) {
-                query = value;
-                result.put(query, new HashMap<String, Double>());
-                numQuery++;
-            } else if (key.equals("url")) {
-                String[] tmps = value.split(" ", 2);
-                url = tmps[0].trim();
-                double score = Double.parseDouble(tmps[1].trim());
-                result.get(query).put(url, score);
-                numDoc++;
+                if (key.equals("query")) {
+                    query = value;
+                    result.put(query, new HashMap<String, Double>());
+                    numQuery++;
+                } else if (key.equals("url")) {
+                    String[] tmps = value.split(" ", 2);
+                    url = tmps[0].trim();
+                    double score = Double.parseDouble(tmps[1].trim());
+                    result.get(query).put(url, score);
+                    numDoc++;
+                }
             }
+            reader.close();
+            System.err.println("# Rel file " + rel_file_name + ": number of queries=" + numQuery + ", number of documents=" + numDoc);
+        } catch (Exception e) {
+            e.printStackTrace();
+            result = null;
         }
-        reader.close();
-        System.err.println("# Rel file " + rel_file_name + ": number of queries=" + numQuery + ", number of documents=" + numDoc);
 
         return result;
     }
 
     public static void main(String[] args) {
-        try {
-            System.out.print(loadRelData(args[0]));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        System.out.print(loadRelData(args[0]));
     }
 }
